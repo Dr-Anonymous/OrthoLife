0b3756907d2e84c5343bc4e3a97cbd63de6c4035
Dr-Anonymous <gangrenesoul@gmail.com>
2025-06-20 13:07:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Index from "./pages/Index";
+import Index from "./pages/AppointmentPage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
