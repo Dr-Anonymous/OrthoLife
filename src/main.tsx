cdd9724b4658284429c963c6374ca5388437f0a8
Dr-Anonymous <gangrenesoul@gmail.com>
2025-07-03 20:24:42 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { handleSPARedirect } from './utils/spa-redirect'
-import { useScrollToHash } from './utils/useScrollToHash'
 // Handle SPA redirect for GitHub Pages
 handleSPARedirect()
-
-useScrollToHash(80); // adjust offset if you have a fixed navbar
 
 createRoot(document.getElementById("root")!).render(<App />);
